284329b32765305946ea803e2278a9dbfc787f75
chalonhutson <chalonhutson@gmail.com>
2021-09-10 15:30:28 -0600

    Finished assignment.

--- markov.py
+++ markov.py
@@ -12,22 +12,12 @@ def open_and_read_file(file_path):
     """
 
     # your code goes here
-    dict = {}
     contents = open(file_path).read()
     words = contents.split()
     # print(words)
-    for i in range(len(words) -1):
-        try:
-            tup = words[i], words[i+1]
-            dict[tup] = words[i+2]
-            print(words[i+2], dict[tup])
-        except IndexError:
-            print("Index Error, breaking loop.")
-            break
+    return words
 
-    print(dict)
-    
-    return 'Contents of your file as one long string'
+
 
 
 def make_chains(text_string):
@@ -56,23 +46,61 @@ def make_chains(text_string):
     """
 
     chains = {}
-
+    words = text_string
     # your code goes here
+
+    for i in range(len(words) - 2):
+            try:
+                key = words[i], words[i+1]
+                if key in chains:
+                        chains[key].append(words[i+2])
+                else:
+                    chains[key] = [words[i+2]]
+
+            except IndexError:
+                print("Index Error, reached the end of the iterator.")
+                pass
+
+    # print(chains)
 
     return chains
 
 
 def make_text(chains):
     """Return text from chains."""
+    key = choice(list(chains.keys()))
+    words = [key[0], key[1]]
+    word = choice(chains[key])
 
-    words = []
+    # Keep looping until we reach a value of None
+    # (which would mean it was the end of our original text)
+    # Note that for long texts (like a full book), this might mean
+    # it would run for a very long time.
+    try:
+        while word is not None:
+            key = (key[1], word)
+            words.append(word)
+            word = choice(chains[key])
+    except KeyError:
+        print("Key error!!")
 
-    # your code goes here
+
+
+
+    # for key in chains:
+    #     chains[key[1], choice(chains.get(key))]
+    #     print(chains)
+
+
+
+    # words = []
+
+    # # your code goes here
 
     return ' '.join(words)
 
 
-input_path = 'green-eggs.txt'
+input_path = 'gettysburg_copy.txt'
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
